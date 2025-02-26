74c7ee8c55644c3b406a4310b52cd7c9fce69a3e
Dimi <rub.dimas@yandex.ru>
2025-02-26 22:10:51 +0300

    task 2 added

--- Games/Game.java
+++ Games/Game.java
@@ -5,6 +5,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static Games.GameProgress.saveGame;
+import static Games.GameProgress.zipFiles;
+
 
 public class Game {
     private static final StringBuilder log = new StringBuilder();
@@ -24,6 +30,21 @@ public class Game {
         createDir("Games/res/vectors");
         createDir("Games/res/icons");
         createFile("Games/temp", "temp.txt");
+
+
+        GameProgress gameProgress1=new GameProgress(10,2,1,2);
+        GameProgress gameProgress2=new GameProgress(11,4,2,3.1 );
+        GameProgress gameProgress3=new GameProgress(13,5,3,4.4);
+        saveGame("Games/savegames/save.dat",gameProgress1);
+        saveGame("Games/savegames/save2.dat",gameProgress2);
+        saveGame("Games/savegames/save3.dat",gameProgress3);
+
+        zipFiles("Games/savegames/save.zip",
+                new ArrayList<>(List.of("Games/savegames/save.dat",
+                        "Games/savegames/save2.dat",
+                        "Games/savegames/save3.dat")));
+
+
         saveLogToFile();
     }
 
@@ -64,7 +85,7 @@ public class Game {
             logMessage("Ошибка записи лога в файл: " + e.getMessage());
         }
     }
-    private static void logMessage(String message) {
+    protected static void logMessage(String message) {
         String timestamp = LocalDateTime.now().format(formatter);
         log.append("[").append(timestamp).append("] ").append(message).append("\n");
     }
---
+++ Games/GameProgress.java
@@ -0,0 +1,100 @@
+package Games;
+
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static Games.Game.logMessage;
+
+public class GameProgress implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int health;
+    private int weapons;
+    private int lvl;
+    private double distance;
+
+    public GameProgress(int health, int weapons, int lvl, double distance) {
+        this.health = health;
+        this.weapons = weapons;
+        this.lvl = lvl;
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "GameProgress{" +
+                "health=" + health +
+                ", weapons=" + weapons +
+                ", lvl=" + lvl +
+                ", distance=" + distance +
+                '}';
+    }
+
+    public static void saveGame(String path, GameProgress gameProgress) {
+        File saveFile = new File(path);
+
+        try (FileOutputStream fos = new FileOutputStream(saveFile);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+            logMessage("Создано сохранение: " + saveFile.getAbsolutePath());
+
+        } catch (IOException e) {
+            logMessage("Ошибка при сохранении игры: " + e.getMessage());
+        }
+    }
+
+    public static void zipFiles(String archivePath, List<String> filesToZip) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(archivePath))) {
+            File filepath = null;
+            for (String filePath : filesToZip) {
+                File file = new File(filePath);
+                filepath= file;
+                if (!file.exists()) {
+                    logMessage("Файл не найден: " + filePath);
+                    continue;
+                }
+
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    ZipEntry entry = new ZipEntry(file.getName());
+                    zout.putNextEntry(entry);
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = fis.read(buffer)) > 0) {
+                        zout.write(buffer, 0, bytesRead);
+                    }
+
+                    zout.closeEntry();
+                    logMessage("Файл добавлен в архив: " + filePath);
+
+                }
+
+            }
+            logMessage("Создан Zip Архив: " + archivePath);
+            deleteFilesExceptZip(filepath.getParent());
+        } catch (IOException e) {
+            logMessage("Ошибка при создании архива: " + e.getMessage());
+        }
+    }
+
+    public static void deleteFilesExceptZip(String dirPath) {
+        File directory = new File(dirPath);
+        if (!directory.exists() || !directory.isDirectory()) {
+            logMessage("Директория не найдена: " + dirPath);
+            return;
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null) return;
+
+        for (File file : files) {
+            if (!file.getName().endsWith(".zip")) {
+                file.delete();
+                logMessage("Удален файл "+file.getName());
+            }
+        }
+
+    }
+}
